966bf7d3c9297d0a6ef640af820d8fe57494b007
monicaaatadeo <monicaaatadeo@gmail.com>
2020-04-10 14:17:46 -0700

    added some urls and views, created forms file

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,30 @@
+from django.urls import path
+from . import views
+
 from django.contrib import admin
-# Add the include function to the import
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # In this case '' represents the root route
     path('', include('main_app.urls')),
+
+    # General Pages
+    path('', views.home, name ='home'),
+    path('about/', views.about, name ='about'),
+
+    # HOUSEMATES
+    path('housemates/', views.housemates_index, name ='index'),
+    path('housemates/new/', views.new_housemate, name ='new_housemate'),
+    path('housemates/<int:housemate_id>/', views.housemates_detail, name ='detail'),
+    path('housemates/<int:housemate_id/edit/', views.housemates_update, name = 'housemates_update'),
+    path('housemates/<int:housemate_id/delete', views.housemates_delete, name ='housemates_delete'),
+
+    # CHORES
+    path('chores/', views.ChoreList.as_view(), name = 'chores_index'),
+    path('chores/<int:pk>/', views.ChoreDetail.as_view(), name = 'chores_detail'),
+    path('chores/create/', views.ChoreCreate.as_view(), name ='chores_create'),
+    path('chores/<int:pk>/update', views.ChoreUpdate.as_view(), name ='chores_update')
+    path('chores/<int:pk>/delete', views.ChoreDelete. as_view(), name ='chores_delete')
+
+
 ]
