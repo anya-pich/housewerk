b5aea6f497f19a25b81b76d4949e3b088ef97e52
anya-pich <anna.pich@outlook.com>
2020-04-14 16:33:55 -0700

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn housewerk.wsgi
--- housewerk/settings.py
+++ housewerk/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +127,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'profile_home'
 
 LOGOUT_REDIRECT_URL = '/'
+django_heroku.settings(locals())
